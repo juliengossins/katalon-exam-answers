efff4f9f02714d3f4a64bc3184707253c8c974d1
Julien <julien.gossins@outlook.com>
2018-09-21 11:47:08 +0200

    Suites added

--- Test Suites/Suite_AddAddresses.ts
+++ Test Suites/Suite_AddAddresses.ts
@@ -4,7 +4,7 @@
    <name>Suite_AddAddresses</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-21T11:26:22</lastRun>
+   <lastRun>2018-09-21T11:44:38</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
--- Test Suites/Suite_DeleteAddesses.ts
+++ Test Suites/Suite_DeleteAddesses.ts
@@ -4,7 +4,7 @@
    <name>Suite_DeleteAddesses</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-21T11:27:55</lastRun>
+   <lastRun>2018-09-21T11:42:58</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
